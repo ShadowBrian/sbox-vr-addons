8793a418931f63283645d0f5a5432b4e222836dc
Brian Lindenhof <brian@shadowbrain.net>
2023-03-05 21:08:10 +0100

    Add hands to FP4s VR controls

--- fp_vr/code/VRControls.cs
+++ fp_vr/code/VRControls.cs
@@ -9,170 +9,182 @@ using System.Threading.Tasks;
 
 public partial class VRControls
 {
-    public static int WeaponIndex = 0;
-    public static bool ChangedWeapon;
+	public static int WeaponIndex = 0;
+	public static bool ChangedWeapon;
 
-    public static bool ReplacedUI;
-    public static bool SpawnedCockpit;
-
-
-    public static AnimatedEntity LeftHand, RightHand;
-
-    public static Vector3 LastFramePosition;
-    public static Vector3 PositionDelta;
+	public static bool ReplacedUI;
+	public static bool SpawnedCockpit;
 
 
-    public static Vector3 originalPosition;
-    public static Rotation originalRotation;
+	public static AnimatedEntity LeftHand, RightHand;
 
-    public static Rotation offsetRotation;
-    public static Vector3 offsetPosition;
-
-    public static Rotation FromToRotation(Vector3 fromDirection, Vector3 toDirection)
-    {
-        Vector3 cross = Vector3.Cross(fromDirection, toDirection);
-        float dot = Vector3.Dot(fromDirection, toDirection);
-        float angle = MathX.RadianToDegree(MathF.Atan2(cross.Length, dot));
-        Vector3 axis = cross.Normal;
-        return Rotation.FromAxis(axis, angle);
-    }
-
-    [Event.Client.Frame]
-    public static void Frame()
-    {
-        if (VR.Enabled && Game.LocalPawn != null)
-        {
-            //activeCockpit.Position = Game.LocalPawn.Position;
-            // activeCockpit.Rotation = Rotation.LookAt(Game.LocalPawn.Rotation.Forward.WithZ(0));
-            VR.Scale = 0.125f;
-            Camera.ZNear = 1f;
-            Transform pos = activeCockpit.Transform;
-            pos.Position += activeCockpit.Rotation.Forward * 23f;
+	public static Vector3 LastFramePosition;
+	public static Vector3 PositionDelta;
 
 
+	public static Vector3 originalPosition;
+	public static Rotation originalRotation;
 
-            if (Input.VR.LeftHand.ButtonA.WasPressed)
-            {
-                //offsetPosition = (Input.VR.Head.Position - VR.Anchor.Position).WithZ(-1f);
-                //offsetPosition = VR.Anchor.NormalToWorld(offsetPosition);
-                //offsetPosition = new Vector3(offsetPosition.y, offsetPosition.x, offsetPosition.z);
+	public static Rotation offsetRotation;
+	public static Vector3 offsetPosition;
 
-                // Calculate the offset between the current headset position and the playspace center
-                offsetPosition = (Input.VR.Head.Position - VR.Anchor.Position).WithZ(-1f);
+	public static Rotation FromToRotation(Vector3 fromDirection, Vector3 toDirection)
+	{
+		Vector3 cross = Vector3.Cross(fromDirection, toDirection);
+		float dot = Vector3.Dot(fromDirection, toDirection);
+		float angle = MathX.RadianToDegree(MathF.Atan2(cross.Length, dot));
+		Vector3 axis = cross.Normal;
+		return Rotation.FromAxis(axis, angle);
+	}
 
-                // Calculate the offset rotation needed to align the headset forward direction with the playspace forward direction
-                Vector3 headsetForward = (Input.VR.Head.Rotation * Vector3.Forward).WithZ(0);
-                Vector3 playspaceForward = VR.Anchor.Rotation * Vector3.Forward;
-                offsetRotation = FromToRotation(headsetForward, playspaceForward);
+	[Event.Client.Frame]
+	public static void Frame()
+	{
+		if (VR.Enabled && Game.LocalPawn != null)
+		{
+			//activeCockpit.Position = Game.LocalPawn.Position;
+			// activeCockpit.Rotation = Rotation.LookAt(Game.LocalPawn.Rotation.Forward.WithZ(0));
+			VR.Scale = 0.125f;
+			Camera.ZNear = 1f;
+			Transform pos = activeCockpit.Transform;
+			pos.Position += activeCockpit.Rotation.Forward * 23f;
 
-                // Rotate the offset position to match the playspace rotation
-                offsetPosition = VR.Anchor.Rotation.Inverse * offsetPosition;
-                offsetPosition = offsetRotation * offsetPosition;
-                offsetPosition = VR.Anchor.Rotation * offsetPosition;
-            }
 
-            if (offsetPosition != Vector3.Zero)
-            {
-                //pos.Rotation *= offsetRotation;
-                //pos.Position -= offsetPosition;
-            }
 
-            VR.Anchor = pos;
+			if (Input.VR.LeftHand.ButtonA.WasPressed)
+			{
+				//offsetPosition = (Input.VR.Head.Position - VR.Anchor.Position).WithZ(-1f);
+				//offsetPosition = VR.Anchor.NormalToWorld(offsetPosition);
+				//offsetPosition = new Vector3(offsetPosition.y, offsetPosition.x, offsetPosition.z);
 
-            PositionDelta = activeCockpit.Position - LastFramePosition;
+				// Calculate the offset between the current headset position and the playspace center
+				offsetPosition = (Input.VR.Head.Position - VR.Anchor.Position).WithZ(-1f);
 
-            LastFramePosition = activeCockpit.Position;
+				// Calculate the offset rotation needed to align the headset forward direction with the playspace forward direction
+				Vector3 headsetForward = (Input.VR.Head.Rotation * Vector3.Forward).WithZ(0);
+				Vector3 playspaceForward = VR.Anchor.Rotation * Vector3.Forward;
+				offsetRotation = FromToRotation(headsetForward, playspaceForward);
 
-            if (SpawnedCockpit)
-            {
-                LeftHand.Transform = RotateTransform(Input.VR.LeftHand.Transform);
-                RightHand.Transform = RotateTransform(Input.VR.RightHand.Transform);
-                LeftHand.Position += PositionDelta;
-                RightHand.Position += PositionDelta;
-            }
-        }
-    }
+				// Rotate the offset position to match the playspace rotation
+				offsetPosition = VR.Anchor.Rotation.Inverse * offsetPosition;
+				offsetPosition = offsetRotation * offsetPosition;
+				offsetPosition = VR.Anchor.Rotation * offsetPosition;
+			}
 
-    public static Transform RotateTransform(Transform trans)
-    {
-        Transform RotatedTransform = trans;
-        Vector3 LocalizedPosition = trans.Position - VR.Anchor.Position;
-        Angles AnchorAngles = new Angles(VR.Anchor.Rotation.Angles().pitch * 0.1f, 0, VR.Anchor.Rotation.Angles().roll);
-        //Vector3 RotatedPosition = LocalizedPosition * AnchorAngles.ToRotation();
-        //RotatedTransform.Position += VR.Anchor.Rotation.Forward * VR.Anchor.Rotation.Angles().pitch * 0.125f;
-        //RotatedTransform.Position += VR.Anchor.Rotation.Up * VR.Anchor.Rotation.Angles().pitch * 0.005f;
-        //RotatedTransform.Position += RotatedPosition;
-        return RotatedTransform;
-    }
+			if (offsetPosition != Vector3.Zero)
+			{
+				//pos.Rotation *= offsetRotation;
+				//pos.Position -= offsetPosition;
+			}
 
-    public static Cockpit activeCockpit;
+			VR.Anchor = pos;
 
-    public static Interactable joy;
-    public static Interactable throt;
+			PositionDelta = activeCockpit.Position - LastFramePosition;
 
-    [Event.Client.BuildInput]
-    public static void BuildInput()
-    {
+			LastFramePosition = activeCockpit.Position;
 
-        if (!SpawnedCockpit)
-        {
-            activeCockpit = Cockpit.FromPrefab("prefabs/cockpit1.prefab");
-            activeCockpit.SetParent(Game.LocalPawn);
-            activeCockpit.LocalPosition = Vector3.Zero;
-            activeCockpit.LocalRotation = Rotation.Identity;
-            foreach (var item in activeCockpit.Children)
-            {
-                if (item is Interactable stick && stick.IsThrottle)
-                {
-                    throt = stick;
-                }
-                else if (item is Interactable stick2)
-                {
-                    joy = stick2;
-                }
-            }
-            LeftHand = new AnimatedEntity("models/vrcontroller.vmdl");
-            LeftHand.SetParent(activeCockpit);
-            RightHand = new AnimatedEntity("models/vrcontroller.vmdl");
-            RightHand.SetParent(activeCockpit);
-            SpawnedCockpit = true;
+			if (SpawnedCockpit)
+			{
+				LeftHand.Transform = RotateTransform(Input.VR.LeftHand.Transform);
+				RightHand.Transform = RotateTransform(Input.VR.RightHand.Transform);
+				LeftHand.Position += PositionDelta;
+				RightHand.Position += PositionDelta;
 
-            Game.TickRate = 30;
-        }
+				RightHand.Scale = 0.1f;
+				LeftHand.Scale = 0.1f;
+			}
+		}
+	}
 
-        if (joy.Parent == null || !activeCockpit.EnableDrawing)
-        {
-            Log.Info("We died!");
-            activeCockpit.Delete();
-            activeCockpit = null;
-            joy = null;
-            SpawnedCockpit = false;
-            return;
-        }
+	public static Transform RotateTransform(Transform trans)
+	{
+		Transform RotatedTransform = trans;
+		Vector3 LocalizedPosition = trans.Position - VR.Anchor.Position;
+		Angles AnchorAngles = new Angles(VR.Anchor.Rotation.Angles().pitch * 0.1f, 0, VR.Anchor.Rotation.Angles().roll);
+		//Vector3 RotatedPosition = LocalizedPosition * AnchorAngles.ToRotation();
+		//RotatedTransform.Position += VR.Anchor.Rotation.Forward * VR.Anchor.Rotation.Angles().pitch * 0.125f;
+		//RotatedTransform.Position += VR.Anchor.Rotation.Up * VR.Anchor.Rotation.Angles().pitch * 0.005f;
+		//RotatedTransform.Position += RotatedPosition;
+		return RotatedTransform;
+	}
 
-        if (activeCockpit == null)
-        {
-            SpawnedCockpit = false;
-        }
+	public static Cockpit activeCockpit;
 
-        if (VR.Enabled)
-        {
+	public static Interactable joy;
+	public static Interactable throt;
 
-            if (Game.LocalPawn != null && joy != null)
-            {
-                Vector2 move = new Vector2(0f, throt.gripped ? -Input.VR.LeftHand.Joystick.Value.x : 0f);
-                Input.AnalogMove = move;
-                Input.AnalogLook = new Angles(joy.GetAnimParameterFloat("forwardback") / 2f, -joy.GetAnimParameterFloat("leftright"), 0);
+	[Event.Client.BuildInput]
+	public static void BuildInput()
+	{
+		if (!ReplacedUI)
+		{
+			//Game.RootPanel.Delete();
+			//new HudVR();
+			ReplacedUI = true;
+		}
 
-                Input.SetButton(InputButton.Forward, throt.GetAnimParameterFloat("forwardback") > 0.5f);
+		if (!SpawnedCockpit)
+		{
+			activeCockpit = Cockpit.FromPrefab("prefabs/cockpit1.prefab");
+			activeCockpit.SetParent(Game.LocalPawn);
+			activeCockpit.LocalPosition = Vector3.Zero;
+			activeCockpit.LocalRotation = Rotation.Identity;
+			foreach (var item in activeCockpit.Children)
+			{
+				if (item is Interactable stick && stick.IsThrottle)
+				{
+					throt = stick;
+				}
+				else if (item is Interactable stick2)
+				{
+					joy = stick2;
+				}
+			}
+			LeftHand = new AnimatedEntity("models/hands/hand_basic_left.vmdl");
 
-                Input.SetButton(InputButton.Back, throt.GetAnimParameterFloat("forwardback") < -0.5f);
+			LeftHand.SetParent(activeCockpit);
+			RightHand = new AnimatedEntity("models/hands/hand_basic.vmdl");
 
-                Input.SetButton(InputButton.Run, throt.gripped && Input.VR.LeftHand.Trigger.Value > 0.75f);
+			RightHand.SetParent(activeCockpit);
+			SpawnedCockpit = true;
+		}
 
-                Input.SetButton(InputButton.PrimaryAttack, joy.gripped && Input.VR.RightHand.Trigger.Value > 0.75f);
-            }
-        }
-    }
+		if (joy.Parent == null || !activeCockpit.EnableDrawing)
+		{
+			Log.Info("We died!");
+			activeCockpit.Delete();
+			activeCockpit = null;
+			joy = null;
+			SpawnedCockpit = false;
+			return;
+		}
+
+		if (activeCockpit == null)
+		{
+			SpawnedCockpit = false;
+		}
+
+		if (VR.Enabled)
+		{
+
+			if (Game.LocalPawn != null && joy != null)
+			{
+				LeftHand.SetAnimParameter("grip", Input.VR.LeftHand.Grip.Value);
+				RightHand.SetAnimParameter("grip", Input.VR.RightHand.Grip.Value);
+
+				Vector2 move = new Vector2(0f, throt.gripped ? -Input.VR.LeftHand.Joystick.Value.x : 0f);
+				Input.AnalogMove = move;
+				Input.AnalogLook = new Angles(joy.GetAnimParameterFloat("forwardback") / 2f, -joy.GetAnimParameterFloat("leftright"), 0);
+
+				Input.SetButton(InputButton.Forward, throt.GetAnimParameterFloat("forwardback") > 0.5f);
+
+				Input.SetButton(InputButton.Back, throt.GetAnimParameterFloat("forwardback") < -0.5f);
+
+				Input.SetButton(InputButton.Run, throt.gripped && Input.VR.LeftHand.Trigger.Value > 0.75f);
+
+				Input.SetButton(InputButton.PrimaryAttack, joy.gripped && Input.VR.RightHand.Trigger.Value > 0.75f);
+			}
+		}
+	}
 }
 
